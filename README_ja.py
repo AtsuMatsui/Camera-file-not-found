3054c611e55106cf9d5d26a73c89b9e1bb4132ea
AtsuMatsui <contact@atsumatsui.com>
2025-05-01 16:58:52 +0900

    Add files via upload

---
+++ README_ja.py
@@ -0,0 +1,6 @@
+カメラが見つかりませんエラー
+<p align="center">
+  <img src="images/Camera_file_not_found.png" alt="required_connections" width="600">
+</p>
+Nukeを開いたときに発生したこのエラーは、カメラノードでファイルが読み込めないときに起こるようです。
+ここではそのファイルパスを変更するPythonコードが見れます。
